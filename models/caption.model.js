08f1c8b90e33fe091cba2b3e1853f8099a848fc8
Raghav Pahwa <raghavpahwa@outlook.com>
2021-05-04 00:13:21 +0530

    Added category to models

--- models/caption.model.js
+++ models/caption.model.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 
 const CaptionSchema = new Schema({
   title: { type: String, required: true, max: 500 },
+  category: { type: String, required: true },
   author: { type: String, required: true },
   likes: { type: Number, default: 0 },
 });
